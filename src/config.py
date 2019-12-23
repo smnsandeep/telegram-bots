9a409717fa353ccc3ba64b341276ef7bed2492db
betabear26 <betabear26@gmail.com>
2019-12-23 10:47:57 +0530

    Reading environment vars

--- src/config.py
+++ src/config.py
@@ -1,18 +1,18 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv(verbose=True)
+#load_dotenv(verbose=True)
 
 class Config(object):
-    SECRET_KEY = os.getenv("FLASK_KEY")
+    SECRET_KEY = os.environ.get("FLASK_KEY", "")
     
 class AppConfig(object):
-    DEBUG = os.getenv("DEBUG")
-    PORT = os.getenv('PORT', 5000)
+    DEBUG = os.environ.get("DEBUG", False)
+    PORT = os.environ.get('PORT', 5000)
 
 class TeleConfig(object):
-    ENTITLED_TOKEN = os.getenv("ENTITLEMENT_BOT_API")
-    VARA_TOKEN = os.getenv("VARA_BOT_API")
+    ENTITLED_TOKEN = os.environ.get("ENTITLEMENT_BOT_API")
+    #VARA_TOKEN = os.getenv("VARA_BOT_API")
 
     ENTITLED_WEBHOOK = "https://telegram-per-bot.herokuapp.com/"
-    VARA_WEBHOOK = ""
+    #VARA_WEBHOOK = ""
