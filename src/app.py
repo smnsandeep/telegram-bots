87f0f751d4ea94b3a2637f9910180f15f75864ba
betabear26 <betabear26@gmail.com>
2019-12-23 10:30:35 +0530

    Setting up the webhook

--- src/app.py
+++ src/app.py
@@ -1,25 +1,29 @@
 from flask import Flask, request
-from config import Config, TeleConfig
+from config import Config, TeleConfig, AppConfig
 import telebot
 
-botEntitled = telebot.Telebot(TeleConfig.ENTITLED_TOKEN, threaded=False)
+botEntitled = telebot.TeleBot(TeleConfig.ENTITLED_TOKEN)
 # botVara = telebot.TeleBot(TeleConfig.VARA_TOKEN)
-
-botEntitled.remove_webhook()
-botEntitled.set_webhook(TeleConfig.ENTITLED_WEBHOOK)
 
 server = Flask(__name__)
 server.config.from_object(Config)
 
-if __name__ == "__main__":
-    server.debug=Config.DEBUG
-    server.run(host='0.0.0.0', port=5000)
-
-@server.route('/entitled', methods=['POST'])
+@server.route('/', methods=['POST'])
 def webhook():
-    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
-    botEntitled.process_new_messages([update.message])
+    botEntitled.remove_webhook()
+    #botEntitled.set_webhook(TeleConfig.ENTITLED_WEBHOOK+TeleConfig.ENTITLED_TOKEN)
+    print(request)
+    return "!", 200
+    
+@server.route('/'+TeleConfig.ENTITLED_TOKEN, methods=['POST'])
+def getMessage():
+    botEntitled.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 @botEntitled.message_handler(commands=['start'])
-def start(m):
-    botEntitled.send_message(m.chat.id, "Welcome, I can now talk to you")
+def start(message):
+    botEntitled.send_message(message.chat.id, f"Welcome {message.from_user.first_name}, I can now talk to you")
+
+if __name__ == "__main__":
+    server.debug=AppConfig.DEBUG
+    server.run(host='0.0.0.0', port=AppConfig.PORT)
--- src/config.py
+++ src/config.py
@@ -5,7 +5,10 @@ load_dotenv(verbose=True)
 
 class Config(object):
     SECRET_KEY = os.getenv("FLASK_KEY")
+    
+class AppConfig(object):
     DEBUG = os.getenv("DEBUG")
+    PORT = os.getenv('PORT', 5000)
 
 class TeleConfig(object):
     ENTITLED_TOKEN = os.getenv("ENTITLEMENT_BOT_API")
