fa177f71bd71c67f6a91327f830a773ba9f9399c
betabear26 <betabear26@gmail.com>
2019-12-23 11:02:22 +0530

    Adding test messages

--- src/app.py
+++ src/app.py
@@ -10,8 +10,6 @@ server.config.from_object(Config)
 
 @server.route('/', methods=['POST'])
 def webhook():
-    botEntitled.remove_webhook()
-    botEntitled.set_webhook(TeleConfig.ENTITLED_WEBHOOK+TeleConfig.ENTITLED_TOKEN)
     print(request)
     return "!", 200
     
@@ -23,6 +21,30 @@ def getMessage():
 @botEntitled.message_handler(commands=['start'])
 def start(message):
     botEntitled.send_message(message.chat.id, f"Welcome {message.from_user.first_name}, I can now talk to you")
+
+@botEntitled.message_handler(commands=['sue'])
+def sue(message):
+    botEntitled.send_message(message.chat.id, f"My momma is a lawyer, Imma sue you {message.from_user.first_name}")
+
+@botEntitled.message_handler(commands=['property'])
+def property(message):
+    botEntitled.send_message(message.chat.id, f"I have 3 houses in 3 cities. Too bad grandma won't give me cuz I'm gay")
+
+@botEntitled.message_handler(commands=['marry'])
+def marry(message):
+    botEntitled.send_message(message.chat.id, f"I will be forced to marry a chick although I am gay and will try to have baby with a lesbian")
+
+@botEntitled.message_handler(commands=['instantnoodles'])
+def instantnoodles(message):
+    botEntitled.send_message(message.chat.id, f"My sister made instant noodles and that is the best food I've ever eaten in my whole life")
+
+@botEntitled.message_handler(commands=['lowcaste'])
+def lowcaste(message):
+    botEntitled.send_message(message.chat.id, f"Hi {message.from_user.first_name}, you are a low caste and you shouldn't be allowed to live")
+
+@botEntitled.message_handler(commands=['momma'])
+def momma(message):
+    botEntitled.send_message(message.chat.id, f"My momma bigggggg lawyer, she make big case.")
 
 if __name__ == "__main__":
     server.debug=AppConfig.DEBUG
