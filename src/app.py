3f954a92be709a54772e67a63942fa13a4e35cf4
betabear26 <betabear26@gmail.com>
2019-12-24 00:08:20 +0530

    Enabling logging

--- src/app.py
+++ src/app.py
@@ -15,15 +15,18 @@ def webhook():
     
 @server.route('/'+TeleConfig.BOT_TOKEN, methods=['POST'])
 def getMessage():
+    server.logger.debug("Incoming request -> " + request)
     butlerBot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
 @butlerBot.message_handler(commands=['start'])
 def start(message):
+    server.logger.debug("start message -> " + message)
     butlerBot.send_message(message.chat.id, Constants.greeting.format(message.from_user.first_name))
 
 @butlerBot.message_handler(commands=['help'])
 def help(message):
+    server.logger.debug("help message -> "+message)
     message = Constants.help
     butlerBot.send_message(message.chat.id, message)
 
