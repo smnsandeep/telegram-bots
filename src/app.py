e1ee103ee879867794109a6c84a9259e473af1f0
betabear26 <betabear26@gmail.com>
2020-01-25 22:36:41 +0530

    Joke v2.0

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ server.config.from_object(Config)
 @server.route('/', methods=['POST'])
 def webhook():
     print(request)
-    return "You are not authorized to visit this page. Your IP has been logged and this incident will be reported. hihihihi",401
+    return "Unauthorised access.",401
     
 @server.route('/'+TeleConfig.BOT_TOKEN, methods=['POST'])
 def getMessage():
